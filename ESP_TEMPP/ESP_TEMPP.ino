8f72bb2f3b1555419da9ced9ad9041e63e98843a
Unknown <dennisbunne92@gmail.com>
2017-09-27 11:31:06 +0200

    DHT code updated

--- ESP_TEMPP/ESP_TEMPP.ino
+++ ESP_TEMPP/ESP_TEMPP.ino
@@ -10,22 +10,20 @@ boolean connectUDP();
 boolean connectWifi();
 
 // wifi connection variables
-const char* ssid = "TN_24GHz_ABB15F";
-const char* password = "2AE679EC96";
+const char* ssid = "Jonathan";
+//const char* password = "";
 boolean wifiConnected = false;
 
-const int DHTpin = 5;
+const int DHTpin = 2;
 int temp;
 int celsius = 0;
-int humidity = 0;
-
-//int value = 0, y = 0; 
+//int humidity = 0;
 
 // UDP variables
 unsigned int localPort = 8888;
 WiFiUDP UDP;
 boolean udpConnected = false;
-IPAddress remote(192,168,10,209);
+IPAddress remote(192,168,0,4);
 char ReplyBuffer[10]; // a string to send back
 
 
@@ -50,36 +48,27 @@ void loop() {
     Serial.print("\n");
     delay(100);
 
-    temp = DHT.read11(DHTpin);
 
-    celsius = DHT.temperature;
-    humidity = DHT.humidity;
-    delay(2000);
-    Serial.print("Tempeartur:");
-    Serial.print(celsius);
-    Serial.print("C");
-    Serial.print("\t|\t");
-    Serial.print("Luftfuktighet:");
-    Serial.print(humidity);
-    Serial.print("%\n");
     
     if(udpConnected)
     {   
 
-      
-      //itoa(celsius, ReplyBuffer, 10);
-      
-      /*for(int i = 0; i <= 10; i++){
-        value += analogRead(A0);
-        delay(10); 
-      }
-      value = value/10;
-      y = map(value, 10, 700, 0, 1024); 
-      value = 0; 
-      itoa(y,ReplyBuffer,10);*/
+    temp = DHT.read11(DHTpin);
+
+    celsius = DHT.temperature;
+    //humidity = DHT.humidity;
+    delay(1000);
+    Serial.print("Tempeartur:");
+    Serial.print(celsius);
+    Serial.print("C");
+   /* Serial.print("\t|\t");
+    Serial.print("Luftfuktighet:");
+    Serial.print(humidity);*/
+    Serial.print("%\n");
+    itoa(celsius, ReplyBuffer, 10);
       
       UDP.beginPacket(remote, localPort);
-      UDP.write("1 ");
+      UDP.write("2 ");
       UDP.write(ReplyBuffer);
       UDP.endPacket();
         delay(100); 
